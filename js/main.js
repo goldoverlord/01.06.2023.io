33bcf57b33fec6f51abda58d3600424a2f2e47a6
vitalii <116792525+goldoverlord@users.noreply.github.com>
2023-06-01 19:39:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$('.js-slick-slider').slick({
+  dots: false,
+  infinite: true,
+  lazyLoad: 'ondemand',
+  swipeToSlide: true,
+  centerMode: true,
+  speed: 400,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
